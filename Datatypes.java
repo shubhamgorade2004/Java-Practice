b3cbb235f8daf0d7cad447fddb7c4d24c55be4bd
Gorade Shubham Rajendra <shubhamgorade2004@gmail.com>
2025-09-25 18:25:17 +0530

    all added suscessfully

---
+++ Datatypes.java
@@ -0,0 +1,48 @@
+class Datatypes {
+    public static void main(String args[]){
+        //integral numbers
+        //byte
+        //short
+        //int
+        //long
+        byte a = 10;
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        short b = 100;
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        int c = 1000;
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        long d = 1000000l;
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        //Decimal Numbers
+        //float
+        //double
+        float salary = 12.12f;
+        double salary2 = 122.12233;
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
+        //character
+        char shu = 'a';
+        System.out.println((int) shu);
+        char shu2 ='2';
+        System.out.println((int) Character.MIN_VALUE);
+        System.out.println((int) Character.MAX_VALUE);
+
+        System.out.println((char) 10084);
+        char hindiChar = 2309;
+        System.out.println(hindiChar);
+
+        //hexadecimal number
+        char heart = '\u2764';
+        System.out.println(heart);
+
+        //Boolean true, false
+        boolean Shubham = true;
+        System.out.println(Shubham);
+    }
+}
